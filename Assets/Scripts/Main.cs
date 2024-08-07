9b5a8da427067a1894eb353df22eaea1105740f7
s3224353 <s3224353@kimurayukiMacBook-Pro.local>
2024-08-07 16:45:36 +0900

    Initial commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    GameObject clickedGameObject;
+    public GameObject m_square;
+    [SerializeField] ParticleSystem m_particle;
+    [SerializeField] GameObject m_BR;
+    [SerializeField] GameObject m_BL;
+    [SerializeField] GameObject m_UR;
+    [SerializeField] GameObject m_UL;
+    [SerializeField] GameObject m_zimen;
+    [SerializeField] GameObject m_PSR;
+    [SerializeField] GameObject m_PSL;
+
+
+    private bool m_particleFlg = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+                ClickObj();
+            }
+        }
+    }
+
+
+    void ClickObj() {
+        string TempName = clickedGameObject.name;
+        switch (TempName)
+        {
+            case "Square":
+                m_square.GetComponent<Square>().ScuareClick();
+                break;
+
+            case "UL":
+                if (m_particleFlg == true)
+                {
+                    m_particle.gameObject.SetActive(false);
+                    m_particleFlg = false;
+                }
+                else {
+                    m_particle.gameObject.SetActive(true);
+                    m_particleFlg = true;
+                }
+                break;
+
+            case "BL":
+                m_BL.SetActive(false);
+                m_UL.SetActive(false);
+                m_BR.SetActive(false);
+                m_UR.SetActive(false);
+                m_zimen.SetActive(false);
+                m_particle.gameObject.SetActive(false);
+                m_PSL.gameObject.SetActive(true);
+                m_PSR.gameObject.SetActive(true);
+
+                break;
+
+            case "UR":
+                m_square.GetComponent<Square>().URClick();
+                break;
+
+            case "BR":
+                m_square.GetComponent<Square>().BRClick();
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    private Animator m_Animator = null;
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+    public void ScuareClick()
+    {
+        m_Animator.SetTrigger("Action");
+    }
+    public void BRClick() {
+        m_Animator.SetTrigger("Cube");
+    }
+    public void URClick()
+    {
+        m_Animator.SetTrigger("Mochi");
+    }
+
+}
